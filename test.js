5d9a7d5d38776882fcf6d2bd2f5c97fbff16fb76
Tolu Bada <142679079+DaRealBada@users.noreply.github.com>
2025-08-18 13:02:38 -0700

    Update test.js
    Signed-off-by: Tolu Bada <142679079+DaRealBada@users.noreply.github.com>

--- test.js
+++ test.js
@@ -8,9 +8,9 @@ app.post('/', (req, res) => {
 doctype
 html
 head
-    title= 'Hello worlv'
+    title= 'Hello world'
 body
-    form(action='/' method='get') // should be change to post method
+    form(action='/' method='get') // should be corrected to post method
         input#name.form-control(type='text)
         button.btn.btn-primary(type='submit') Submit
     p Hello `+ input
